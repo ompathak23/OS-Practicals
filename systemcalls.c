c01eac2e6bb50de6c2f8f47563b61c0be3661c1c
Om Pathak <ompatthak@gmail.com>
2021-04-16 10:37:15 +0530

    System Call 2.0

--- systemcalls.c
+++ systemcalls.c
@@ -6,24 +6,35 @@
 #include <sys/wait.h>
 int main()
 {
-pid_t pid;
-int ret = 1;
-int status;
-pid = fork();
-if (pid == -1)
-{
-printf("can't fork, error occured\n");
-exit(EXIT_FAILURE);
+    pid_t pid;
+    int ret = 1;
+    int status;
+    pid = fork();
+    if (pid == -1){
+        printf("can't fork, error occured\n");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0){
+        printf("child process, pid = %u\n", getpid());
+        printf("parent of child process, pid = %u\n", getppid());
+        char *argv_list[] = {"ls", "-lart", "/home", NULL};
+        execv("ls", argv_list);
+        exit(0);
+    }
+    else{
+        printf("Parent Of parent process, pid = %u\n", getppid());
+        printf("parent process, pid = %u\n", getpid());
+    if (waitpid(pid, &status, 0) > 0){
+        if (WIFEXITED(status) && !WEXITSTATUS(status)) printf("program execution successful\n");
+        else if (WIFEXITED(status) && WEXITSTATUS(status)){
+            if (WEXITSTATUS(status) == 127) printf("execv failed\n");
+            else printf("program terminated normally,"" but returned a non-zero status\n");
+    else printf("program didn't terminate normally\n");
+    else printf("waitpid() failed\n");
+
+    exit(0);
+
+    }
+    
+    return 0;
 }
-else if (pid == 0)
-{
-printf("child process, pid = %u\n", getpid());
-printf("parent of child process, pid = %u\n", getppid());
-char *argv_list[] = {"ls", "-lart", "/home", NULL};
-execv("ls", argv_list);
-exit(0);
-}
-else
-{
-printf("Parent Of parent process, pid = %u\n", getppid());
-printf("parent process, pid = %u\n", getpid());
